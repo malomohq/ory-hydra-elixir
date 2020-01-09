9a70a76a743418f14cbda2c610ac65ba73724974
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-01-09 14:32:08 -0500

    Implemented hackney HTTP client

---
+++ lib/ory/hydra/client/hackney.ex
@@ -0,0 +1,38 @@
+defmodule ORY.Hydra.Client.Hackney do
+  alias ORY.Hydra.{ Client }
+
+  @behaviour ORY.Hydra.Client
+
+  @spec request(ORY.Hydra.http_method_t(), String.t(), ORY.Hydra.http_headers_t(), String.t(), any)
+           :: { :ok, ORY.Hydra.Client.response_t() } | { :error, any }
+  def request(method, url, headers, body, opts) do
+    opts = opts ++ [:with_body]
+
+    response =
+      :hackney.request(
+        method,
+        url,
+        headers,
+        body,
+        opts
+      )
+
+      package(response)
+  end
+
+  defp package({ :ok, status_code, headers }) do
+    response = %{ body: "", headers: headers, status_code: status_code }
+
+    { :ok, response }
+  end
+
+  defp package({ :ok, status_code, headers, body }) do
+    response = %{ body: body, headers: headers, status_code: status_code }
+
+    { :ok, response }
+  end
+
+  defp package(otherwise) do
+    otherwise
+  end
+end
