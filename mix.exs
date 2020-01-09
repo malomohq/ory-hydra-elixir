a05b3f13b42b6f02038e80b4888419b4be21bb94
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-01-09 13:11:28 -0500

    Set dialyzer core PLT path to _build/#{Mix.env()}

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ORY.Hydra.MixProject do
       version: "0.0.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -23,11 +24,17 @@ defmodule ORY.Hydra.MixProject do
       { :jason, "~> 1.1", optional: true },
 
       #
-      #
+      # dev
       #
 
       { :dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false },
       { :ex_doc, ">= 0.0.0", only: :dev, runtime: false }
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_core_path: "./_build/#{Mix.env()}"
     ]
   end
 end
