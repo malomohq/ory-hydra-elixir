822eca224a909ad5f861f73289b5a95d4a0bfd6c
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-07-17 15:25:59 -0400

    Bumped version to 1.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ORY.Hydra.MixProject do
   def project do
     [
       app: :ory_hydra,
-      version: "1.1.1",
+      version: "1.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
