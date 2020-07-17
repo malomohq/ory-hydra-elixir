0772e704cf10bd8848fc7dd29d164cb5381903b7
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-07-17 08:21:00 -0400

    Support encoding body as x-www-form-urlencoded

--- lib/ory/hydra/helpers/body.ex
+++ lib/ory/hydra/helpers/body.ex
@@ -4,9 +4,16 @@ defmodule ORY.Hydra.Helpers.Body do
     ""
   end
 
-  def encode!(operation, config) do
+  def encode!(%{ content_type: :json } = operation, config) do
     operation.params
     |> Map.drop(operation.params_in_query)
     |> config.json_codec.encode!()
+  end
+
+  def encode!(%{ content_type: :form_urlencoded } = operation, _config) do
+    operation.params
+    |> Map.drop(operation.params_in_query)
+    |> URI.encode_query()
+    |> URI.encode_www_form()
   end
 end
--- test/ory/hydra/helpers/body_test.exs
+++ test/ory/hydra/helpers/body_test.exs
@@ -10,4 +10,12 @@ defmodule ORY.Hydra.Helpers.BodyTest do
 
     assert "{\"ok\":true}" == Helpers.Body.encode!(%Operation{ method: :post, params: %{ ok: true, not_ok: false }, params_in_query: [:not_ok] }, Config.new(%{}))
   end
+
+  test "encode!/2 when :content_type is :form_urlencoded" do
+    assert "ok%3Dtrue" == Helpers.Body.encode!(%Operation{ content_type: :form_urlencoded, method: :post, params: %{ ok: true } }, Config.new(%{}))
+  end
+
+  test "encode!/2 when :content_type is :json" do
+    assert "{\"ok\":true}" == Helpers.Body.encode!(%Operation{ content_type: :json, method: :post, params: %{ ok: true } }, Config.new(%{}))
+  end
 end
