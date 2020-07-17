68c177249c087ebb62b4b2492f7fa853bc4013ef
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-07-17 08:11:41 -0400

    Specify content type to use

--- lib/ory/hydra/operation.ex
+++ lib/ory/hydra/operation.ex
@@ -1,11 +1,16 @@
 defmodule ORY.Hydra.Operation do
   @type t ::
           %__MODULE__{
+            content_type: :form_urlencoded | :json,
             method: ORY.Hydra.http_method_t(),
             params: map,
             params_in_query: list(atom),
             path: String.t()
           }
 
-  defstruct method: nil, params: %{}, params_in_query: [], path: nil
+  defstruct content_type: :json,
+            method: nil,
+            params: %{},
+            params_in_query: [],
+            path: nil
 end
