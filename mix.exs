abf0353e1f30e5f7ebdb5e526a6e08760a6fa613
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-08-07 14:43:30 -0400

    Clean up

--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule ORY.Hydra.MixProject do
   defp deps do
     [
       { :hackney, "~> 1.15", optional: true },
+
       { :jason, "~> 1.1", optional: true },
 
       #
@@ -29,6 +30,7 @@ defmodule ORY.Hydra.MixProject do
       #
 
       { :dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false },
+
       { :ex_doc, ">= 0.0.0", only: :dev, runtime: false },
 
       #
